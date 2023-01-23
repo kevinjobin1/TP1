d4abd7db3751004c16e463f19f510122edec2420
Kevin Jobin <kevin.jobin.1@ulaval.ca>
2023-01-23 16:46:11 -0500

    feat: initialize profil and url in main activity

--- app/src/main/java/ca/ulaval/ima/tp1/MainActivity.kt
+++ app/src/main/java/ca/ulaval/ima/tp1/MainActivity.kt
@@ -2,11 +2,14 @@ package ca.ulaval.ima.tp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val monProfil: Profil = Profil("Jobin", "Kevin", GregorianCalendar(1994, 12, 29).time, "536777791")
+        val urlToLoad : String = "https://google.com"
 
     }
 }
